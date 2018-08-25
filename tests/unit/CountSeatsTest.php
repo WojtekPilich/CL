9d641a1422823cc64fd3f3061a945a6b2d35249a
Wojtek Pilich <chilip@tlen.pl>
2018-08-25 20:54:24 +0200

    unit tests

--- tests/unit/CountSeatsTest.php
+++ tests/unit/CountSeatsTest.php
@@ -5,7 +5,15 @@ class myTestCountSeats extends PHPUnit\Framework\TestCase
     public function testCountSeats()
     {
         $example1 = new \App\CountSeats(['A' => 30000, 'B' => 45000, 'C' => 8000], 35);
-        $this->assertEquals($example1->countResult(), ['A' => 13, 'B' => 19, 'C' => 3 ]);
-    }
 
+        $this->assertEquals($example1->countResult(), ['A' => 13, 'B' => 19, 'C' => 3 ]);
+        $this->assertNotEquals($example1->countResult(), ['A' => 11, 'B' => 17, 'C' => 22 ]);
+
+        $this->assertInternalType('integer', $example1->getSeats());
+        $this->assertInternalType('array', $example1->getPartyVotes());
+
+        $this->assertInstanceOf('\App\CountSeats', $example1);
+
+        $this->assertArrayHasKey('C', $example1->getPartyVotes());
+    }
 }
