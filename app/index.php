16b34c9361f3163f76692da4321b5cca94423e18
Wojtek Pilich <chilip@tlen.pl>
2018-08-25 14:08:24 +0200

    almost

--- app/index.php
+++ app/index.php
@@ -2,4 +2,77 @@
 
 namespace App;
 
+class CountSeats
+{
+    protected $totalSeats;
+    protected $result;
+    protected $partyVotes = [];
+    protected $data = [];
+
+    public function __construct($votes, $seats)
+    {
+        $this->partyVotes = $votes;
+        $this->totalSeats = $seats;
+    }
+
+    public function countFraction()
+    {
+        $sum = 0;
+        foreach ($this->partyVotes as $party => $votes) {
+            $sum += $votes;
+        }
+
+        $firstStep = [];
+        foreach ($this->partyVotes as $party => $votes) {
+            $firstStep[$party] = round(($votes * $this->totalSeats) / $sum, 4);
+        }
+        //fraction
+        $secondStep = [];
+        foreach ($firstStep as $first => $num) {
+            $secondStep[$first] = $num - floor($num);
+        }
+        asort($secondStep);
+        //int
+        $thirdStep = [];
+        foreach ($firstStep as $first => $num) {
+            $thirdStep[$first] = floor($num);
+        }
+
+        $sumSeats = array_sum($thirdStep);
+        $remainingSeats = $this->totalSeats - $sumSeats;
+
+        $testTest = array_slice($secondStep, -$remainingSeats, $remainingSeats, true);
+
+        $fourStep = [];
+        foreach ($thirdStep as $key => $val) {
+            foreach ($testTest as $key1) {
+                if($key = $key1) {
+                    $val += 1;
+                }
+            }
+            $fourStep[] = $key[$val];
+        }
+
+//        return $fourStep;
+//        return $testTest;
+//        return $firstStep;
+//        return $secondStep;
+//        return $thirdStep;
+
+    }
+
+    public function getPartyVotes()
+    {
+        return $this->partyVotes;
+    }
+
+    public function getSeats()
+    {
+        return $this->totalSeats;
+    }
+
+}
+
+$test = new CountSeats(['B' => 5400, 'A' => 15000, 'C' => 5500, 'D' => 5550], 15);
+print_r($test->countFraction());
 
